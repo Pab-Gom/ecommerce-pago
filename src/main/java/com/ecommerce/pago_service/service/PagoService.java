c881e745228ea37f141defcb4aac1b57bdd83cbe
Pab-Gom <pab.gomezm@duocuc.cl>
2026-05-11 19:44:41 -0400

    Update PagoService.java
    editando PagoService

--- src/main/java/com/ecommerce/pago_service/service/PagoService.java
+++ src/main/java/com/ecommerce/pago_service/service/PagoService.java
@@ -55,7 +55,6 @@ public class PagoService {
 
         log.info("Pago creado con id {}", guardado.getId());
 
-        // 🔥 6. Simular pago exitoso
         guardado.setEstado("PAGADO");
         pagoRepository.save(guardado);
 
